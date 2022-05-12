c7f57ffb907f16fa9937edc9a160fad809be7b17
yaroslav.koval <yaroslav@koval.page>
2022-05-12 21:12:46 +0200

    simple update

--- file_api/extract_link.py
+++ file_api/extract_link.py
@@ -1,45 +1,47 @@
-import aiohttp
+import requests_cache
 import config
 import logging as log
 from aiogram.types import Message
+from datetime import timedelta
 
 
 class Extractor:
     def __init__(self, file_id: str, msg: Message = None) -> None:
+        self.session = requests_cache.CachedSession(
+            'cache_extractor', backend='memory', expire_after=timedelta(minutes=30)
+        )
         self.file_id = file_id
         self.msg = msg
 
     @property
-    async def get_file_data(self) -> dict or None:
+    def get_file_data(self) -> dict or None:
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                        "https://api.telegram.org/bot%s/getFile" % config.BOT_TOKEN,
-                        params={"file_id": self.file_id}
-                ) as response:
-                    r = await response.json()
-                    return r["result"]
+            response = self.session.get(
+                "https://api.telegram.org/bot%s/getFile" % config.BOT_TOKEN,
+                params={"file_id": self.file_id}
+            )
+            return response.json()["result"]
         except Exception as e:
             log.error("%s: %s" % (self.get_file_data.__name__, e))
 
-    async def check_file_data(self, data: dict) -> str or None:
+    def check_file_data(self, data: dict) -> str or None:
         try:
             return data["file_path"]
         except Exception as e:
             log.error("%s: %s" % (self.check_file_data.__name__, e))
 
-    async def check_size(self, file_data: dict) -> bool:
+    def check_size(self, file_data: dict) -> bool:
         return True if 20971520 > file_data["file_size"] else False
 
     @property
-    async def file_data_prepare(self) -> str:
+    def file_data_prepare(self) -> str:
         try:
-            resp = await self.get_file_data
-            if await self.check_size(resp):
-                data = await self.check_file_data(resp)
+            resp = self.get_file_data
+            if self.check_size(resp):
+                data = self.check_file_data(resp)
                 return data["file_path"]
             else:
-                await self.msg.reply(
+                self.msg.reply(
                     "Max size file is 20 megabytes. Read this - https://core.telegram.org/bots/api#file"
                 )
         except Exception as e:
@@ -47,5 +49,5 @@ class Extractor:
         return "Unknown error"
 
     @property
-    async def build_link(self) -> str:
+    def build_link(self) -> str:
         return "%s/%s" % (config.HOST, self.file_id)
