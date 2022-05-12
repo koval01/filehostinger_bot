4bd5f5df8ed83ee48e61d30138d3297b7dc25304
yaroslav.koval <yaroslav@koval.page>
2022-05-12 14:44:30 +0200

    update web

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn file_api.http_server:app
+web: gunicorn --workers=5 file_api.http_server:app
 worker: python bot.py
--- file_api/http_server.py
+++ file_api/http_server.py
@@ -1,34 +1,18 @@
-import aiohttp
+from flask import Flask, request, stream_with_context
+from requests import get as http_get
+import config
 
-from aiohttp import web
-from aiohttp import streamer
-
-
-@streamer
-async def file_sender(writer, file=None):
-    with open(file, 'rb') as f:
-        chunk = f.read(2 ** 16)
-        while chunk:
-            await writer.write(chunk)
-            chunk = f.read(2 ** 16)
+app = Flask(__name__)
 
 
-async def download_file(request):
-    headers = {
-        "Content-disposition": "inline"
-    }
-    return web.Response(
-        body=file_sender(file_path=await download_file_from_tg()),
-        headers=headers
+@app.route('/<path:type_file>/<regex(".*"):file_name>', methods=['GET'])
+def get_file(type_file: str, file_name: str):
+    response = http_get(
+        'https://api.telegram.org/file/bot%s/%s/%s' % (
+            config.BOT_TOKEN, type_file, file_name
+        ), stream=True,
+        headers={
+            'user-agent': request.headers.get('user-agent')
+        }
     )
-
-
-async def download_file_from_tg(url: str) -> bytes:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if resp.status == 200:
-                return resp.content.read()
-
-
-app = web.Application()
-app.router.add_get('/file/{file_name}', download_file)
+    return app.response_class(stream_with_context(response.raw))
