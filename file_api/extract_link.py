f978b5a82d7b44f58fd6c2e81c942ae32afdd3bf
yaroslav.koval <yaroslav@koval.page>
2022-05-12 21:43:10 +0200

    simple update

--- file_api/extract_link.py
+++ file_api/extract_link.py
@@ -4,24 +4,24 @@ import logging as log
 from aiogram.types import Message
 from datetime import timedelta
 
+session = requests_cache.CachedSession(
+    'cache_extractor', backend='memory', expire_after=timedelta(minutes=30)
+)
+
 
 class Extractor:
     def __init__(self, msg: Message = None, photo: bool = False, file_id: str = None) -> None:
-        self.session = requests_cache.CachedSession(
-            'cache_extractor', backend='memory', expire_after=timedelta(minutes=30)
-        )
         if msg:
             self.photo = msg.photo[-1:][0] if photo else None
             self.file_id, self.file_size = (self.photo.file_id, self.photo.file_size) if photo else (
                 eval("msg.%s.%s" % (msg.content_type, e)) for e in ["file_id", "file_size"]
             )
-        else:
-            self.file_id = file_id
+        else: self.file_id = file_id
 
     @property
     def get_file_data(self) -> dict or None:
         try:
-            response = self.session.get(
+            response = session.get(
                 "https://api.telegram.org/bot%s/getFile" % config.BOT_TOKEN,
                 params={"file_id": self.file_id}
             )
